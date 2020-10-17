7c72ab28b579ef14fa4c82e4f8d794d3a5a28b09
Stepan <48178543+Step7439@users.noreply.github.com>
2020-10-17 12:05:09 +0900

    Edite

--- ESP8266_DHT11/main.ino
+++ ESP8266_DHT11/main.ino
@@ -28,7 +28,7 @@ DHT dht2(5, DHT11);
 // as the current DHT reading algorithm adjusts itself to work on faster procs.
 
 
-//DHT dht(DHTPIN, DHTTYPE); step ddddd
+//DHT dht(DHTPIN, DHTTYPE);
 //DHT dht1(DHTPIN, DHTTYPE);
 
 #define wifi_ssid "AABCDE"
